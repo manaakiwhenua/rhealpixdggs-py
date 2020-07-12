a9bc5ce27052c46612721a1cff486e0bf8e6549f
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-07-12 20:56:38 +1000

    update setup.py for release

--- setup.py
+++ setup.py
@@ -1,22 +1,60 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+import codecs
+import os
+VERSION = "0.5.1"
 
+
+def open_local(paths, mode="r", encoding="utf8"):
+  path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    *paths
+  )
+  return codecs.open(path, mode, encoding)
+
+
+with open_local(["README.rst"], encoding="utf-8") as readme:
+  long_description = readme.read()
+
+
+with open_local(["requirements.txt"]) as req:
+  install_requires = req.read().split("\n")
+
+  
 setup(
-  name='rHEALPixDGGS',
-  version='0.5.1',
-  author='Alexander Raichev',
-  author_email='alex@raichev.net',
-  packages=['rhealpix_dggs', 'rhealpix_dggs.tests'],
-  url='http://code.scenzgrid.org/index.php/p/scenzgrid-py/',
-  license='LICENSE.txt',
-  long_description=open('README.rst').read(),
-  description='An implementation of the rHEALPix discrete global grid system',
-  install_requires=[
-    'python >= 3.3',
-    'numpy >= 1.7',
-    'scipy >= 0.12', 
-    'matplotlib >= 1.2.1', 
-    'pyproj >= 1.9.3',
+  name="rHEALPixDGGS",
+  version=VERSION,
+  author="Alexander Raichev",
+  author_email="alex@raichev.net",
+  packages=find_packages(),
+  package_dir={"rhealpixdggs": "rhealpixdggs"},
+  url="https://github.com/manaakiwhenua/rhealpixdggs-py",
+  download_url="https://github.com/manaakiwhenua/rhealpixdggs-py/archive/v{:s}.tar.gz".format(VERSION),
+  license="LICENSE.txt",
+  description="An implementation of the rHEALPix discrete global grid system",
+  long_description=long_description,
+  long_description_content_type="text/x-rst",
+  keywords=["DGGS", "Discreet Global Grid System", "reference system", "spatial", "geospatial"],
+  project_urls={
+    "Bug Reports": "https://github.com/manaakiwhenua/rhealpixdggs-py/issues",
+    "Source": "https://github.com/manaakiwhenua/rhealpixdggs-py",
+  },
+  install_requires=install_requires,
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Topic :: Utilities",
+    "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Software Development :: Libraries :: Python Modules",
   ],
 )
