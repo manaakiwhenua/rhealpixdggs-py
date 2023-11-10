be79ca15736b52081efc80806994e31028a39e74
Nicoletta De Maio <demaion@landcareresearch.co.nz>
2023-11-10 15:02:20 +1300

    Added docstrings (and TODOs) to functions in rhp wrapper script

--- rhealpixdggs/rhp_wrappers.py
+++ rhealpixdggs/rhp_wrappers.py
@@ -3,6 +3,21 @@ from rhealpixdggs.dggs import WGS84_003
 
 
 def geo_to_rhp(lat: float, lng: float, resolution: int, plane: bool = True) -> str:
+    """
+    Turn a latitute and longitude (in degrees) into an rHEALPix cell address at
+    the requested resolution.
+
+    Uses the predefined WGS84_003 DGGS with the WGS84 ellipsoid and n = 3 to
+    subdivide the cell sides.
+
+    Mostly passes through the parameters to the function turning coordinate points
+    into cells, but converts the address tuple from the resulting cell into a
+    string.
+
+    TODO: give the option to select another predefined DGGS, or pass in a custom one
+    TODO: give the option to set n to something other than 3
+    TODO: give the option to enter the coordinates in radians
+    """
     # Get the grid cell corresponding to the coordinates
     cell = WGS84_003.cell_from_point(resolution, (lng, lat), plane)
 
@@ -39,8 +54,22 @@ def rhp_to_parent(rhpindex: str, res: int = None, verbose: bool = True) -> str:
 def rhp_to_geo_boundary(
     rhpindex: str, geo_json: bool = True, plane: bool = True
 ) -> tuple[tuple[float, float]]:
-    # Grab the cell vertices as tuple((lat, lng), (lat, lng), (lat, lng), (lat, lng))
-    # (Includes non-corner point in darts if plane == False)
+    """
+    Extract the corner coordinates of a cell at a given cell ID and returns them as
+    a tuple of coordinate pairs (in degrees).
+
+    If geojson is requested as the output format:
+        - Will return (longitude, latitude) coordinate pairs.
+        - Will repeat the first vertex and insert it at the end if geojson is requested as
+          the output format.
+
+    If geojson is NOT requested as the output format:
+        - Will return (latitude, longitude) coordinate pairs in order to be consistent with
+          h3 coordinate ordering.
+
+    TODO: give the option of requesting corner coordinates in radians
+    """
+    # Grab the cell vertices (includes non-corner point in darts if plane == False)
     suid = [int(d) if d.isdigit() else d for d in rhpindex]
     cell = WGS84_003.cell(suid)
     verts = tuple(cell.vertices(plane=plane))
